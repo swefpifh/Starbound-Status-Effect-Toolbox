0758677d598065d7d2ffc8a0a15847c3c962ad9e
swefpifh <90806985+swefpifh@users.noreply.github.com>
2024-06-08 22:35:22 +0200

    Add files via upload

---
+++ sadRedHeadArrow/sadRedHeadArrow.lua
@@ -0,0 +1,11 @@
+function init()
+  animator.setAnimationState("arrowState", "visible")
+end
+
+function update(dt)
+  -- Pas besoin de mise à jour spécifique ici
+end
+
+function uninit()
+  -- Nettoyage si nécessaire
+end
